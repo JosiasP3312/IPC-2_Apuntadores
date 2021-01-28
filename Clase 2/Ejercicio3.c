30b0c51280f48bcd81024e8d9abc77b2393243d0
JosiasP3312 <josiasperez@icloud.com>
2021-01-28 07:42:52 -0600

    Ejercicio 3
    Creacion de ejercicio 3

---
+++ Clase 2/Ejercicio3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Referenciacion: Es la obtención de la dirección de una variable
+
+int main() {
+    int value = 18;
+    int *ptr_1, *ptr_2;
+    ptr_1=&value;
+    ptr_2 = ptr_1;
+    
+    printf("La dirección de memoria de ptr_2 = %p", ptr_2);
+    
+    return 0;
+}
